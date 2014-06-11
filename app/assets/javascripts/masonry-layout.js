55f0f4476fcdd5d16782ed162a33d07de2d55f24
jphager2 <jphager2@gmail.com>
2014-06-11 11:35:53 +0200

    masonry should be working, but it is not

---
+++ app/assets/javascripts/masonry-layout.js
@@ -0,0 +1,10 @@
+$( document ).ready( function(){
+  $( '#masonry-container' ).masonry( 
+    { 
+      columnWidth: 80,
+      itemSelector: '.box' 
+    } 
+  ).imagesLoaded(function(){
+    $( '#masonry-container' ).masonry('reload');
+  });
+});
