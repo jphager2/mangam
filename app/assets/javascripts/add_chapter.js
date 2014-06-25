f4d6af729c5ca3f816bde4ab60cad97cf328da34
jphager2 <jphager2@gmail.com>
2014-06-25 16:31:29 +0200

    added auto fill for mangareader

--- app/assets/javascripts/add_chapter.js
+++ app/assets/javascripts/add_chapter.js
@@ -6,3 +6,38 @@ function showThis(parentIdent) {
   $(parentIdent).children().show();
 };
 
+var input;
+var match;
+
+$(document).ready( function(){
+  hideThis(".startHidden");
+
+  $( "#chapter_image_url" ).keyup(function() {
+    function checkUrl() {
+      input = $( "#chapter_image_url" );
+
+      function checkMatch(match) {
+        return (match && match.length >= 3);
+      };
+
+      function insertValues(match) {
+        var manga = match[1].replace(/\-/g,' ');
+        var chapter = match[2];
+        $( "#chapter_manga"  ).val(manga);
+        $( "#chapter_number" ).val(chapter);
+      };
+      
+      function mangaReader() {
+        //if it matches a regexp
+        reg = /(?:mangareader\.net\/)((?:\w|\-)+)\/(\d+)/;
+        match = input.val().match(reg); 
+        if (checkMatch(match)) {insertValues(match);}
+      };
+
+      // Execute regex functions
+      mangaReader();
+    };
+    checkUrl();
+    showThis('.startHidden');
+  });
+});
