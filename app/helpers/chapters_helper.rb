f5cf5122b2b9ffb1b063b68e76e915203e937094
jphager2 <jphager2@gmail.com>
2015-04-10 13:55:06 +0200

    working on floating like and tag buttons

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,5 +1,25 @@
 module ChaptersHelper
 
+  def like_to(chapter, text = t('like.verb'))
+    if current_user.has_like(chapter) 
+      if block_given?
+        link_to unlike_path(chapter), method: :delete do 
+          yield
+        end
+      else
+        link_to text, unlike_path, method: :delete
+      end
+    else
+      if block_given?
+        link_to like_path(chapter), method: :post do 
+          yield
+        end
+      else
+        link_to text, like_path, method: :post
+      end
+    end
+  end
+
   def set_current_page!
     @page = params[:page] || 1
     @page = Integer(@page)
@@ -16,8 +36,9 @@ module ChaptersHelper
   end
 
   class NullDashboard 
-    attr_reader :genres, :description, :scanlators
+    attr_reader :genres, :description, :scanlators, :title
     def initialize
+      @title       = ""
       @genres      = []
       @description = "Looks like we can't find this manga"
       @scanlators  = []
