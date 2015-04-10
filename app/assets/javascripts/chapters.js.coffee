c6f2662a4e6d2b4b32d768c7c2293cb27e2c7cb2
jphager2 <jphager2@gmail.com>
2015-04-10 15:27:58 +0200

    floating buttons for chapter show

--- app/assets/javascripts/chapters.js.coffee
+++ app/assets/javascripts/chapters.js.coffee
@@ -1,3 +1,28 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+add_on_hover_handler = ->
+  $('.floating-button.add-button').unbind('hover')
+  $('.floating-button.add-button').hover(
+    (
+      ->
+        $('.hover-area').removeClass('hidden')
+        $('.floating-button.show-on-hover').removeClass('hidden')
+        $('.hover-area').hover(
+          -> $('.floating-button.show-on-hover').removeClass('hidden'),
+          (
+            ->
+              $('.hover-area').addClass('hidden')
+              $('.floating-button.show-on-hover').addClass('hidden')
+          )
+        )
+    ),
+    (-> return false)
+  )
+
+$(document).on 'ready page:load', ->
+  add_on_hover_handler()
+
+$(document).ajaxComplete ->
+  add_on_hover_handler()
--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,21 +1,23 @@
 module ChaptersHelper
 
-  def like_to(chapter, text = t('like.verb'))
-    if current_user.has_like(chapter) 
+  def like_to(chapter, text = nil)
+    if like = current_user.has_like(chapter) 
+      text ||= t('unlike.verb')
+      options = { method: :delete, remote: true }
       if block_given?
-        link_to unlike_path(chapter), method: :delete do 
-          yield
+        link_to(unlike_path(like, format: :js), options) do
+          yield(text, :unlike)
         end
       else
-        link_to text, unlike_path, method: :delete
+        link_to(text, unlike_path(like, format: :js), options)
       end
     else
+      text ||= t('like.verb')
+      options = { method: :post, remote: true }
       if block_given?
-        link_to like_path(chapter), method: :post do 
-          yield
-        end
+        link_to(like_path(chapter), options) { yield(text, :like) }
       else
-        link_to text, like_path, method: :post
+        link_to(text, like_path(chapter), options)
       end
     end
   end
@@ -30,14 +32,17 @@ module ChaptersHelper
   end
 
   def get_dashboard(manga)
-    BU::Api.new.dashboard(manga)
+    timeout(8) do
+      BU::Api.new.dashboard(manga)
+    end
   rescue Object 
     NullDashboard.new
   end
 
   class NullDashboard 
-    attr_reader :genres, :description, :scanlators, :title
+    attr_reader :genres, :description, :scanlators, :title, :url
     def initialize
+      @url         = ""
       @title       = ""
       @genres      = []
       @description = "Looks like we can't find this manga"
